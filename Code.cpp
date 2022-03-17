1f5879da0593ab1f4cbae262594f842313f26296
Monac0 <79639527+Monac0@users.noreply.github.com>
2022-03-17 10:21:20 +0900

    deal with dx1==dx2==0

--- Code.cpp
+++ Code.cpp
@@ -8,8 +8,8 @@
 #include <sstream>
 #include <vector>
 
-const int D = 10;
-const int CPI = 200; //mm
+const int D = 72; //mm
+const int CPI = 1200;
 
 float R = 180 / (3.14159265358979323846 * D * CPI);
 
@@ -20,24 +20,27 @@ float dmove[20000][2];
 float move[20000][2];
 
 void theta_converter(int dx1, int dy1, int dx2, int dy2, int buttonState, int index) {
+	if (dx1 == 0 && dx2 == 0)
+	{
+		theta[index] = theta[index - 1];
+		dmove[index][0] = 0;
+		dmove[index][1] = 0;
+		move[index][0] = move[index - 1][0];
+		move[index][1] = move[index - 1][1];
+
+		return;
+	}
+
 	float delta_theta = (dx1 + dx2) * R;
 	theta[index] = theta[index - 1] + (delta_theta / 2);
 
-	dmove[index][0] = cos(theta[index]) * dx1 - sin(theta[index]) + dy1;
+	dmove[index][0] = cos(theta[index]) * dx1 - sin(theta[index]) * dy1;
 	dmove[index][1] = sin(theta[index]) * dx1 + cos(theta[index]) * dy1;
 	move[index][0] = move[index - 1][0] + (delta_theta / (2 * sin(delta_theta / 2))) * dmove[index][0];
 	move[index][1] = move[index - 1][1] + (delta_theta / (2 * sin(delta_theta / 2))) * dmove[index][1];
 
 	theta[index] = theta[index] + delta_theta / 2;
 }
-
-/*
-for (int row = 1; row < 20000; row++) {
-		theta_converter(rand() % 10 + 1, rand() % 10 + 1, rand() % 10 + 1, rand() % 10 + 1, 0, row);
-
-}
-*/
-
 
 // application reads from the specified serial port and reports the collected data
 int main()
@@ -97,7 +100,9 @@ int main()
 	}
 
 	for (int i = 0; i < 1000; i++)
-		std::cout << "x: " << move[i][0] << ", y: " << move[i][1] << ", theta: " << theta[i] << std::endl;
+		std::cout << std::setw(5) << i << ") " << "x: " << std::setw(20) << move[i][0]
+		<< ", y: " << std::setw(20) << move[i][1]
+		<< ", theta: " << std::setw(20) << theta[i] << std::endl;
 
 	return 0;
 }
