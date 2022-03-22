00a106031a7a6c012c564e1261b8ef6e506fa8c2
Monac0 <79639527+Monac0@users.noreply.github.com>
2022-03-22 10:32:17 +0900

    one byte processing

--- Code.cpp
+++ Code.cpp
@@ -5,23 +5,19 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
-#include <sstream>
 #include <vector>
 #include <fstream>
 
-const float D = 2.834646; //inch
+const double D = 2.834646; //inch
 const int CPI = 1200;
-const float PI = 3.14159265358979323846;
-const float R = 180.0 / (PI * D * CPI);
+const double PI = 3.14159265358979323846;
+const double R = 180.0 / (PI * D * CPI);
 
-float move[2][2]; // [ [xi, yi] , [xi+1, yi+1] ….   ]
-float dmove[2][2]; // [ [dxi, dyi] , [dxi+1, dyi+1] ….   ]
-float theta[2];
+double move[2][2]; // [ [xi, yi] , [xi+1, yi+1] ….   ]
+double dmove[2][2]; // [ [dxi, dyi] , [dxi+1, dyi+1] ….   ]
+double theta[2];
 
-float degree_to_rad(float degree)
-{
-	return degree * PI / 180;
-}
+double degree_to_rad(double degree);
 
 void theta_converter(int dx1, int dy1, int dx2, int dy2, int buttonState1, int buttonState2, int index) {
 	int previousIndex = (index + 1) % 2;
@@ -37,7 +33,7 @@ void theta_converter(int dx1, int dy1, int dx2, int dy2, int buttonState1, int b
 		return;
 	}
 
-	float delta_theta = (dx1 + dx2) * R;
+	double delta_theta = static_cast<double>(dx1 + dx2) * R;
 	theta[index] = theta[previousIndex] + (delta_theta / 2.0);
 
 	dmove[index][0] = cos(degree_to_rad(theta[index])) * dx1 - sin(degree_to_rad(theta[index])) * dy1;
@@ -57,90 +53,99 @@ int main()
 	if (SP->IsConnected())
 		std::cout << "We're connected\n" << std::endl;
 
-	char incomingData[256] = "";			// don't forget to pre-allocate memory
-	//printf("%s\n",incomingData);
-	int dataLength = 255;
+	char incomingData[2] = "";			// don't forget to pre-allocate memory
 	int readResult = 0;
 	
+	/* file to output
 	std::ofstream fout("/Users/KimDongWoo/Documents/output.txt");
 	if (!fout.is_open())
 	{
 		std::cout << "output file not opened" << std::endl;
 		return 1;
 	}
-	
-	
+	*/
 
-	std::stringstream ss;
-	ss.clear();
-	ss.str("");
-	long micro;
-	int dx1, dy1, dx2, dy2;
+	int dx1(0), dy1(0), dx2(0), dy2(0);
 	int button[2] = { 0, 0 };
 	int cnt = 0;
+	std::string inputState = "";
+	std::string num = "0";
 
-	if (SP->IsConnected()) //to ignore first line of data which is a bug time to time
+	/* to ignore first line of data which is a bug time to time
+	if (SP->IsConnected())
 		while(readResult != 0)
 			readResult = SP->ReadData(incomingData, 100);
+	*/
 
 	while (SP->IsConnected())
 	{
-		readResult = SP->ReadData(incomingData, sizeof(incomingData)-1);
-		incomingData[readResult] = '\0';
+		readResult = SP->ReadData(incomingData, 1);
 
 		if (readResult != 0)
 		{
-			ss << incomingData;
-
-			//printing incoming data
-			//std::cout << incomingData << std::endl;
+			char ch = incomingData[0];
+			//std::cout << ch;
 			
-			while (ss.tellg() != -1 && ss.str().size() - ss.tellg() >= 70)
+			switch (ch)
 			{
-				ss >> micro;
-				while (micro < 1000000 && ss.tellg() != -1) //micro의 범위를 알아내서 구체화할 필요 있음
-					ss >> micro;
-
-				ss >> dx1 >> dy1 >> dx2 >> dy2 >> button[0] >> button[1];
-				cnt = (cnt + 1) % 2;
-
-				//printing received values
-				//std::cout << micro << " " << ss.tellg() << " " << dx1 << " " << dy1 << " " << dx2 << " " << dy2 << " " << button[0] << " " << button[1] << std::endl;
-
-				if (ss.tellg() == -1)
-					theta_converter(0, 0, 0, 0, 0, 0, cnt);
+			case 'x':
+			case 'y':
+			case 'c':
+				inputState = ch;
+				break;
+			case 'a':
+			case 'b':
+				inputState += ch;
+				break;
+			default:
+				if (inputState == "n") num += ch;
 				else
-					theta_converter(dx1, dy1, -dx2, dy2, button[0], button[1], cnt);
+				{
+					if (inputState == "xa")
+					{
+						button[1] = stoi(num);
+						cnt = (cnt + 1) % 2;
+						theta_converter(dx1, dy1, -dx2, dy2, button[0], button[1], cnt);
+						//std::cout << " " << dx1 << " " << dx2 << " " << dy1 << " " << dy2 << " " << button[0] << " " << button[1] << std::endl;
 
-				
-				std::cout << std::setw(10) << micro << ") " << "x: " << std::setw(20) << move[cnt][0]
-					<< ", y: " << std::setw(20) << move[cnt][1]
-					<< ", theta: " << std::setw(20) << theta[cnt] << std::endl;
-				
-				
-				fout << micro << " " << move[cnt][0] << " " << move[cnt][1] << " " << theta[cnt] << std::endl;
-			}
+						
+						std::cout << "x: " << std::setw(10) << move[cnt][0]
+							<< ", y: " << std::setw(10) << move[cnt][1]
+							<< ", theta: " << std::setw(10) << theta[cnt] << std::endl;
+						
+												
+					}
+					else if (inputState == "xb") dx1 = stoi(num);
+					else if (inputState == "ya") dx2 = stoi(num);
+					else if (inputState == "yb") dy1 = stoi(num);
+					else if (inputState == "ca") dy2 = stoi(num);
+					else if (inputState == "cb") button[0] = stoi(num);
 
-			int curPos = ss.tellg();
-			if (curPos == -1)
-			{
-				std::cout << "Reaching the end of buffer, please wait a minute" << std::endl;
-				ss.str("");
-				ss.clear();
+					inputState = "n";
+					num = ch;
+				}
 			}
-			else
-			{
-				ss.str(ss.str().substr(ss.tellg()));
-				ss.seekp(0, std::ios::end);
-			}
-				
+			
+			//std::cout << inputState;
+
+			//print to file
+			//fout << move[cnt][0] << " " << move[cnt][1] << " " << theta[cnt] << std::endl;
 
 			//If right-clicked, break the loop
-			if (button[1] == 1) break;
+			if (button[1] == 1)
+			{
+				std::cout << "right clicked, break the loop" << std::endl;
+				break;
+			}
 		}
 	}
 
-	if (fout) fout.close();
-
+	//if (fout) fout.close();
+	std::cout << "program is terminating" << std::endl;
 	return 0;
+}
+
+double degree_to_rad(double degree)
+{
+	return degree * PI / 180;
 }
