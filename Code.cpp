ee41a3c13b3cbca78875394ddd4bda04e8dda81e
Monac0 <79639527+Monac0@users.noreply.github.com>
2022-03-31 15:48:09 +0900

    데이터 단위 인치로 변경

--- Code.cpp
+++ Code.cpp
@@ -1,49 +1,37 @@
 #include <stdio.h>
-#include <tchar.h>
-#include "SerialClass.h"	// Library described above
-#include <string>
 #include <iostream>
+#include <winsock2.h> // 윈속 헤더 포함 
+#include <windows.h> 
+#include <tchar.h>
+#include <string>
 #include <iomanip>
 #include <cmath>
-#include <sstream>
 #include <vector>
 #include <fstream>
+#include "SerialClass.h"	// Library described above
 
-const float D = 2.834646; //inch
+#pragma comment (lib,"ws2_32.lib") // 윈속 라이브러리 링크
+#define BUFFER_SIZE 64 // 버퍼 사이즈 default=1024
+
+const double D = 2.834646; //inch
 const int CPI = 1200;
+const double PI = 3.14159265358979323846;
+const double R = 180.0 / (PI * D * CPI);
 
-const float R = 180.0 / (3.14159265358979323846 * D * CPI);
+double l1 = 500000, l2 = 500000, l3 = 3;//링크의 길이이므로 무조건 길이를 설정해야 한다.
+double x, y;
+double a1, a2;
+double degree1, degree2, degree3;//각도 1,2,3
 
-float theta[2];
-// [ [dxi, dyi] , [dxi+1, dyi+1] ….   ]
-float dmove[2][2];
-// [ [xi, yi] , [xi+1, yi+1] ….   ]
-float move[2][2];
+double move[2][2]; // [ [xi, yi] , [xi+1, yi+1] ….   ]
+double dmove[2][2]; // [ [dxi, dyi] , [dxi+1, dyi+1] ….   ]
+double theta[2];
 
-void theta_converter(int dx1, int dy1, int dx2, int dy2, int buttonState1, int buttonState2, int index) {
-	int previousIndex = (index + 1) % 2;
+double degree_to_rad(double degree);
+double rad_to_degree(double rad);
+void theta_converter(int dx1, int dy1, int dx2, int dy2, int buttonState1, int buttonState2, int index);
 
-	if (dx1 + dx2 == 0)
-	{
-		theta[index] = theta[previousIndex];
-		dmove[index][0] = 0.0;
-		dmove[index][1] = 0.0;
-		move[index][0] = move[previousIndex][0];
-		move[index][1] = move[previousIndex][1];
-
-		return;
-	}
-
-	float delta_theta = (dx1 + dx2) * R;
-	theta[index] = theta[previousIndex] + (delta_theta / 2.0);
-
-	dmove[index][0] = cos(theta[index]) * dx1 - sin(theta[index]) * dy1;
-	dmove[index][1] = sin(theta[index]) * dx1 + cos(theta[index]) * dy1;
-	move[index][0] = move[previousIndex][0] + (delta_theta / (2.0 * sin(delta_theta / 2.0))) * dmove[index][0];
-	move[index][1] = move[previousIndex][1] + (delta_theta / (2.0 * sin(delta_theta / 2.0))) * dmove[index][1];
-
-	theta[index] = theta[index] + delta_theta / 2.0;
-}
+void _2dof_inversekinematics(double x, double y);
 
 // application reads from the specified serial port and reports the collected data
 int main()
@@ -54,90 +42,183 @@ int main()
 	if (SP->IsConnected())
 		std::cout << "We're connected\n" << std::endl;
 
-	char incomingData[256] = "";			// don't forget to pre-allocate memory
-	//printf("%s\n",incomingData);
-	int dataLength = 255;
+	char incomingData[2] = "";			// don't forget to pre-allocate memory
 	int readResult = 0;
-	
-	std::ofstream fout("/Users/KimDongWoo/Documents/output.txt");
-	if (!fout.is_open())
-	{
-		std::cout << "output file not opened" << std::endl;
-		return 1;
-	}
-	
-	
 
-	std::stringstream ss;
-	ss.clear();
-	ss.str("");
-	long micro;
-	int dx1, dy1, dx2, dy2;
+	//make socket
+	WSADATA wsaData; // 윈속 데이터 구조체
+	SOCKET ClientSocket; // 소켓 선언
+	SOCKADDR_IN ToServer; // 서버로 보내는 주소정보 구조체
+
+	int Send_Size;
+	ULONG   ServerPort = 61557; // 서버 포트번호
+
+	char Buffer[BUFFER_SIZE] = {};
+	//sprintf_s(Buffer, "STOP,%.2lf\n", dd);
+	if (WSAStartup(0x202, &wsaData) == SOCKET_ERROR)
+	{
+		std::cout << "WinSock 초기화부분에서 문제 발생 " << std::endl;
+		WSACleanup();
+		exit(0);
+	}
+
+	memset(&ToServer, 0, sizeof(ToServer));
+
+	ToServer.sin_family = AF_INET;
+	ToServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ToServer.sin_port = htons(ServerPort); // 포트번호
+
+	ClientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);//
+
+	if (ClientSocket == INVALID_SOCKET)
+	{
+		std::cout << "소켓을 생성할수 없습니다." << std::endl;
+		closesocket(ClientSocket);
+		WSACleanup();
+		exit(0);
+	}
+
+	int dx1(0), dy1(0), dx2(0), dy2(0);
 	int button[2] = { 0, 0 };
 	int cnt = 0;
-
-	if (SP->IsConnected()) //to ignore first line of data which is a bug time to time
-		while(readResult != 0)
-			readResult = SP->ReadData(incomingData, 100);
+	std::string inputState = "";
+	std::string num = "0";
+	move[0][0] = move[0][1] = 0;
 
 	while (SP->IsConnected())
 	{
-		readResult = SP->ReadData(incomingData, sizeof(incomingData)-1);
-		incomingData[readResult] = '\0';
+		readResult = SP->ReadData(incomingData, 1);
 
 		if (readResult != 0)
 		{
-			ss << incomingData;
-
-			//printing incoming data
-			//std::cout << incomingData << std::endl;
+			char ch = incomingData[0];
+			//std::cout << ch;
 			
-			while (ss.tellg() != -1 && ss.str().size() - ss.tellg() >= 70)
+			switch (ch)
 			{
-				ss >> micro;
-				while (micro < 1000000 && ss.tellg() != -1) //micro의 범위를 알아내서 구체화할 필요 있음
-					ss >> micro;
-
-				ss >> dx1 >> dy1 >> dx2 >> dy2 >> button[0] >> button[1];
-				cnt = (cnt + 1) % 2;
-
-				//printing received values
-				//std::cout << micro << " " << ss.tellg() << " " << dx1 << " " << dy1 << " " << dx2 << " " << dy2 << " " << button[0] << " " << button[1] << std::endl;
-
-				if (ss.tellg() == -1)
-					theta_converter(0, 0, 0, 0, 0, 0, cnt);
+			case 'x':
+			case 'y':
+			case 'c':
+				inputState = ch;
+				break;
+			case 'a':
+			case 'b':
+				inputState += ch;
+				break;
+			default:
+				if (inputState == "n") num += ch;
 				else
-					theta_converter(dx1, dy1, -dx2, dy2, button[0], button[1], cnt);
+				{
+					if (inputState == "xa")
+					{
+						button[1] = stoi(num);
+						cnt = (cnt + 1) % 2;
+						theta_converter(dx1, dy1, -dx2, dy2, button[0], button[1], cnt);
+						//std::cout << " " << dx1 << " " << dx2 << " " << dy1 << " " << dy2 << " " << button[0] << " " << button[1] << std::endl;
 
-				
-				std::cout << std::setw(10) << micro << ") " << "x: " << std::setw(20) << move[cnt][0]
-					<< ", y: " << std::setw(20) << move[cnt][1]
-					<< ", theta: " << std::setw(20) << theta[cnt] << std::endl;
-				
-				
-				fout << micro << " " << move[cnt][0] << " " << move[cnt][1] << " " << theta[cnt] << std::endl;
-			}
+						_2dof_inversekinematics(move[cnt][0], move[cnt][1]);
+						std::cout << "x: " << std::setw(10) << move[cnt][0]
+							<< ", y: " << std::setw(10) << move[cnt][1]
+							<< ", theta1: " << std::setw(10) << degree1
+							<< " theta2: " << std::setw(10) << degree2 << std::endl;
+						
+						/*std::cout << "x: " << std::setw(10) << move[cnt][0]
+							<< ", y: " << std::setw(10) << move[cnt][1]
+							<< ", theta: " << std::setw(10) << theta[cnt] << std::endl;
+						*/
 
-			int curPos = ss.tellg();
-			if (curPos == -1)
-			{
-				std::cout << "Reaching the end of buffer, please wait a minute" << std::endl;
-				ss.str("");
-				ss.clear();
+						//send packet
+						sprintf_s(Buffer, "%lf %lf %lf \n", move[cnt][0], move[cnt][1], theta[cnt]);
+
+						Send_Size = sendto(ClientSocket, Buffer, BUFFER_SIZE, 0,
+							(struct sockaddr*)&ToServer, sizeof(ToServer));
+
+						// 패킷송신시 에러처리
+						if (Send_Size != BUFFER_SIZE)
+						{
+							std::cout << "sendto() error!" << std::endl;
+							exit(0);
+						}
+												
+					}
+					else if (inputState == "xb") dx1 = stoi(num);
+					else if (inputState == "ya") dx2 = stoi(num);
+					else if (inputState == "yb") dy1 = stoi(num);
+					else if (inputState == "ca") dy2 = stoi(num);
+					else if (inputState == "cb") button[0] = stoi(num);
+
+					inputState = "n";
+					num = ch;
+				}
 			}
-			else
-			{
-				ss.str(ss.str().substr(ss.tellg()));
-				ss.seekp(0, std::ios::end);
-			}
-				
+			
+			//std::cout << inputState;
 
 			//If right-clicked, break the loop
-			if (button[1] == 1) break;
+			if (button[1] == 1)
+			{
+				std::cout << "right clicked, break the loop" << std::endl;
+				break;
+			}
 		}
 	}
 
-	if (fout) fout.close();
+	//프로그램 종료 전 "END"가 담긴 패킷을 보냄
+	sprintf_s(Buffer, "END ");
 
+	Send_Size = sendto(ClientSocket, Buffer, BUFFER_SIZE, 0,
+		(struct sockaddr*)&ToServer, sizeof(ToServer));
+
+	closesocket(ClientSocket); //소켓 닫기
+	WSACleanup();
+
+	std::cout << "program is terminating" << std::endl;
 	return 0;
+}
+
+double degree_to_rad(double degree)
+{
+	return degree * PI / 180;
+}
+
+double rad_to_degree(double rad)
+{
+	return (rad * 180 / PI);
+}
+
+void theta_converter(int dx1, int dy1, int dx2, int dy2, int buttonState1, int buttonState2, int index) {
+	int previousIndex = (index + 1) % 2;
+
+	if (dx1 + dx2 == 0) //no rotation
+	{
+		theta[index] = theta[previousIndex];
+		dmove[index][0] = 0.0;
+		dmove[index][1] = 0.0;
+		move[index][0] = move[previousIndex][0];
+		move[index][1] = move[previousIndex][1];
+
+		return;
+	}
+
+	double delta_theta = (static_cast<double>(dx1) + static_cast<double>(dx2)) * R;
+	theta[index] = theta[previousIndex] + (delta_theta / 2.0);
+
+	dmove[index][0] = cos(degree_to_rad(theta[index])) * dx1 - sin(degree_to_rad(theta[index])) * dy1;
+	dmove[index][1] = sin(degree_to_rad(theta[index])) * dx1 + cos(degree_to_rad(theta[index])) * dy1;
+	move[index][0] = move[previousIndex][0] + ((delta_theta / (2.0 * sin(degree_to_rad(delta_theta / 2.0)))) * dmove[index][0])/1200.0;
+	move[index][1] = move[previousIndex][1] + ((delta_theta / (2.0 * sin(degree_to_rad(delta_theta / 2.0)))) * dmove[index][1])/1200.0;
+
+	theta[index] = theta[index] + delta_theta / 2.0;
+}
+
+void _2dof_inversekinematics(double x, double y)//기구학을 이용한 좌표계산
+{
+	double k = ((pow(x, 2) + pow(y, 2) - pow(l1, 2) - pow(l2, 2)) / (2 * l1 * l2));
+	a2 = (atan2(sqrt(1 - pow(k, 2)), k));
+	a1 = atan2(y, x) - atan2(l2 * sin(a2), l1 + l2 * cos(a2));
+
+	a1 *= 180 / PI;//radian to degree
+	a2 *= 180 / PI;//radian to degree
+	degree1 = a1;
+	degree2 = a2;
 }
---
+++ visual.py
@@ -0,0 +1,55 @@
+import pygame
+import sys
+import time
+
+import socket 
+SCREEN_WIDTH = 640 * 2
+SCREEN_HEIGHT = 480 * 2
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+pygame.init()
+pygame.display.set_caption("Mouse track")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+img = pygame.image.load('C:/Users/KimDongWoo/Downloads/cpp-master/mouse.png')
+img = pygame.transform.scale(img, (555/6, 976/6))
+
+
+clock = pygame.time.Clock()
+screen.fill(white)
+
+if __name__ == '__main__':
+ 
+    serverAddressPort = ("127.0.0.1", 61557) 
+    bufferSize = 1024 
+    # 클라이언트 쪽에서 UDP 소켓 생성
+
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPClientSocket.bind(serverAddressPort) 
+
+
+
+    while True:
+        clock.tick(1000)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        msg = msgFromServer[0].decode('unicode_escape').encode('utf-8')
+        msg = msg.decode('utf-8')
+        temp = msg.split(' ')
+        screen.fill(white)
+        
+        if(temp[0] == 'END'):
+            print("Program is terminating")
+            quit()
+            
+        if(msg) :
+            pos_x, pos_y , rotate = float(temp[0]), float(temp[1]), float(temp[2])
+            clock.tick(1000)
+            rotated = pygame.transform.rotate(img, -rotate)
+            player_rect =img.get_rect()
+            current_pos= (SCREEN_WIDTH / 4 + pos_x, SCREEN_HEIGHT / 4 + pos_y)
+            screen.blit(rotated, current_pos)
+            pygame.display.update()
